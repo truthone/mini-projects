0eb63c33eafb3d2ae4f55eb85e1f03663403703b
Jinsil Han <truth.h406@gmail.com>
2022-04-03 04:26:28 +0900

    ✨ [add] 게임결과 , 다시하기 기능 추가

--- rsp.js
+++ rsp.js
@@ -1,5 +1,13 @@
-const list = document.querySelector('#rspImg > ul');
-let count = 0;
+window.addEventListener('DOMContentLoaded',()=>{
+  list = document.querySelector('#rspImg > ul');
+  startGame();
+})
+
+function startGame(){
+  count = 0;
+  slide();
+  timerId = setInterval(slide, 300);
+}
 
 function slide(){
   count++;
@@ -15,6 +23,63 @@ function slide(){
   list.style.transform = `translateY(-${size}px)`;
 }
 
-slide();
-let timerId = setInterval(slide, 300);
+const btns= document.querySelector('.buttons');
+btns.addEventListener('click', e => {
+  clearInterval(timerId);
+  const enemyState = list.children[count].className;
+  console.log(e.target);
+  const myState = e.target.className;
+  console.log(`${count}${enemyState} ${myState}`);
+  const result =  getGameResult(enemyState, myState);
+  consoleResult(result);
+});
+
+function getGameResult(enemyState, myState){
+  switch(myState){
+    case "rock" : 
+      if (enemyState === "scissors"){
+        return 1;
+      }else if (enemyState === "paper"){
+        return -1;
+      }else{
+        return 0;
+      }
+
+    case "scissors" : 
+    if (enemyState === "paper"){
+      return 1;
+    }else if (enemyState === "rock"){
+      return -1;
+    }else{
+      return 0;
+    }
+
+    case "paper" : 
+    if (enemyState === "rock"){
+      return 1;
+    }else if (enemyState === "scissors"){
+      return -1;
+    }else{
+      return 0;
+    }    
+  }
+}
+
+function consoleResult(result){
+  document.querySelector('#result').style.display = 'block';
+  const resultDiv = document.querySelector('#result > div');
+
+  if(result == 0){
+    resultDiv.textContent = "비겼습니다 !";
+  }else if (result == 1) {
+    resultDiv.textContent = "이겼습니다!@@!~~ !";
+  }else if (result == -1){
+    resultDiv.textContent = "졌습니다 ㅠㅠ";
+  }
+}
+document.querySelector('#retry').addEventListener('click', ()=>{
+  document.querySelector('#result').style.display = 'none';
+  startGame();
+});
+
 
